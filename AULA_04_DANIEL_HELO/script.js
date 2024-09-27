17523f30f3fd9ef4a58b267fd11e5b42d09bbf10
Daniel <daniviana2912@gmail.com>
2024-09-27 08:43:43 -0300

    Add files via upload

---
+++ AULA_04_DANIEL_HELO/script.js
@@ -0,0 +1,15 @@
+//laço while = enquanto
+var x = 0;
+document.write('<br>testando loop com while');
+while(x<=10){
+    document.write('<br> O valor de x é: '+x);
+    x = x+1;
+}
+
+//laço for = para
+var x = 0;
+document.write('<br>testando loop com for');
+while(x<=10){
+    document.write('<br> O valor de x é: '+x);
+    x = x+1;
+}
